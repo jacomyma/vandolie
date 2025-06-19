5c4702860672886793e28c3e8430be3336981e91
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-06-19 13:55:17 +0200

    Implements "Semantic map" page

--- src/components/app/Semantic.tsx
+++ src/components/app/Semantic.tsx
@@ -1,5 +1,103 @@
-import type { FC } from "react";
+import { SigmaContainer, useRegisterEvents, useSigma } from "@react-sigma/core";
+import { type FC, useEffect, useMemo, useState } from "react";
+
+import { useAppContext } from "../../core/context.ts";
+import { type DocumentNode, getGraph, useEmbedding, useExtractor, useUMAP } from "../../core/semantic.ts";
+
+const GraphEventsController: FC<{
+  activeNodeId: string | undefined;
+  setActiveNode: (activeNode: DocumentNode | null) => void;
+}> = ({ activeNodeId, setActiveNode }) => {
+  const sigma = useSigma<DocumentNode>();
+  const registerEvents = useRegisterEvents<DocumentNode>();
+
+  useEffect(() => {
+    registerEvents({
+      clickStage: () => {
+        setActiveNode(null);
+      },
+      clickNode: (e) => {
+        const node = e.node;
+
+        setActiveNode(node === activeNodeId ? null : sigma.getGraph().getNodeAttributes(node));
+      },
+    });
+  }, [activeNodeId, setActiveNode, registerEvents, sigma]);
+
+  return null;
+};
 
 export const SemanticComponent: FC = () => {
-  return <>TODO: Semantic</>;
+  const { dataset } = useAppContext();
+  const [selectedNode, setSelectedNode] = useState<DocumentNode | null>(null);
+  const extractor = useExtractor();
+  const { embeddedDocuments, embeddingProgress } = useEmbedding(dataset, extractor);
+  const { projectedDocuments, projectionProgress } = useUMAP(embeddingProgress === 1 ? embeddedDocuments : undefined);
+  const graph = useMemo(() => getGraph(projectedDocuments || undefined), [projectedDocuments]);
+
+  return (
+    <main>
+      <div className="container bg-body pb-4">
+        <div className="container pt-4">
+          <h1>Semantic map</h1>
+
+          {/* Load bars (in a card) */}
+          <div className="card">
+            <div className="card-header">🧠 Compute</div>
+            <div className="card-body">
+              <div className="progress mb-1" role="progressbar">
+                <div
+                  className="progress-bar"
+                  style={{ width: embeddingProgress * 100 + "%" }}
+                  id="embeddings-progress-bar"
+                >
+                  {embeddedDocuments.length}/{dataset.documents.length} documents embedded
+                </div>
+              </div>
+              <div className="progress" role="progressbar">
+                <div className="progress-bar" style={{ width: projectionProgress * 100 + "%" }} id="umap-progress-bar">
+                  {projectionProgress * 100}% 2D projection
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Viz */}
+          <div className="d-flex flex-row mt-3">
+            {/* Sigma */}
+            <SigmaContainer<DocumentNode>
+              graph={graph}
+              className="flex-fill"
+              style={{ height: 500, backgroundColor: "#DDD" }}
+              settings={{
+                nodeReducer(node, data) {
+                  return node === selectedNode?.id ? { ...data, highlighted: true } : data;
+                },
+              }}
+            >
+              <GraphEventsController activeNodeId={selectedNode?.id} setActiveNode={setSelectedNode} />
+            </SigmaContainer>
+
+            {/* Text preview */}
+            <div className="flex-wrap ps-3" style={{ height: 500, width: 240, overflowY: "hidden" }}>
+              {selectedNode && (
+                <div className="h-100 overflow-y-auto">
+                  <h3>{selectedNode.document.title}</h3>
+                  <p>
+                    <span className="badge text-bg-secondary">Category {selectedNode.document.category}</span>
+                  </p>
+                  <p>{selectedNode.document.text}</p>
+                </div>
+              )}
+              {!selectedNode && !!projectedDocuments?.length && (
+                <div className="h-100 p-4 d-flex align-items-center justify-content-center">
+                  <p className="text-center text-muted">You can click on a document, to see its content.</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
---
+++ src/core/semantic.ts
@@ -0,0 +1,169 @@
+import { getAsyncMemoData, useAsyncMemo } from "@ouestware/hooks";
+import { FeatureExtractionPipeline, pipeline } from "@xenova/transformers";
+import Graph from "graphology";
+import { keyBy, sortBy } from "lodash";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import type { Coordinates } from "sigma/types";
+import { UMAP } from "umap-js";
+
+import type { Dataset, Document } from "./consts.ts";
+
+type EmbeddedDocument = {
+  document: Document;
+  features: number[];
+};
+type ProjectedDocument = EmbeddedDocument & {
+  coordinates: Coordinates;
+};
+export type DocumentNode = {
+  id: string;
+  label: string;
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+  document: Document;
+};
+export type DocumentsGraph = Graph<DocumentNode>;
+
+export function useExtractor() {
+  const state = useAsyncMemo(() => pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2", { quantized: false }), []);
+
+  return getAsyncMemoData(state);
+}
+
+export function useEmbedding(dataset: Dataset, extractor?: FeatureExtractionPipeline) {
+  const [state, setState] = useState<{
+    documentsToProcess: Document[];
+    embeddedDocuments: { document: Document; features: number[] }[];
+  }>({ documentsToProcess: dataset.documents, embeddedDocuments: [] });
+
+  const embedFirstDocument = useCallback(() => {
+    if (!extractor) return;
+
+    setState((prevState) => {
+      if (prevState.documentsToProcess.length === 0) return prevState;
+
+      const [firstDoc, ...remainingDocs] = prevState.documentsToProcess;
+
+      extractor(firstDoc.text, { pooling: "mean", normalize: true }).then((features) => {
+        setState((currentState) => ({
+          ...currentState,
+          embeddedDocuments: [
+            ...currentState.embeddedDocuments,
+            { document: firstDoc, features: Array.from(features.data) },
+          ],
+        }));
+      });
+
+      // Return state with document moved from queue
+      return {
+        documentsToProcess: remainingDocs,
+        embeddedDocuments: prevState.embeddedDocuments,
+      };
+    });
+  }, [extractor]);
+
+  useEffect(() => {
+    setTimeout(() => embedFirstDocument(), 0);
+  }, [state, embedFirstDocument]);
+
+  return {
+    embeddedDocuments: state.embeddedDocuments,
+    embeddingProgress: state.embeddedDocuments.length / dataset.documents.length,
+  };
+}
+
+export function useUMAP(embeddedDocuments?: { document: Document; features: number[] }[]) {
+  const [state, setState] = useState<{
+    progress: number;
+    projection?: number[][];
+  }>({
+    progress: 0,
+  });
+  const epochs = useMemo(() => Math.min(1000, 50 * (embeddedDocuments?.length || 0)), [embeddedDocuments]);
+  const umap = useMemo(
+    () =>
+      embeddedDocuments?.length
+        ? new UMAP({
+            nComponents: 2,
+            minDist: 0.1,
+            nNeighbors: Math.min(8, (embeddedDocuments?.length || 0) - 1),
+            nEpochs: epochs,
+          })
+        : null,
+    [embeddedDocuments, epochs],
+  );
+  const projectedDocuments = useMemo<null | ProjectedDocument[]>(() => {
+    const { projection } = state;
+    if (!projection || !embeddedDocuments) return null;
+
+    return embeddedDocuments.map(({ document, features }, i) => ({
+      document,
+      features,
+      coordinates: {
+        x: projection[i][0],
+        y: projection[i][1],
+      },
+    }));
+  }, [embeddedDocuments, state]);
+
+  useEffect(() => {
+    if (!embeddedDocuments?.length || !umap) return;
+
+    umap
+      .fitAsync(
+        embeddedDocuments.map(({ features }) => features),
+        (epochNumber) => {
+          if (epochNumber % 100 == 0 || epochNumber == epochs)
+            setState((state) => ({
+              ...state,
+              progress: epochNumber / epochs,
+            }));
+        },
+      )
+      .then((projection) => {
+        setState({
+          progress: 1,
+          projection,
+        });
+      });
+  }, [embeddedDocuments, umap]);
+
+  return {
+    projectionProgress: state.progress,
+    projectedDocuments,
+  };
+}
+
+export function getGraph(
+  projectedDocuments?: ProjectedDocument[],
+  {
+    palette = ["#777acd", "#cab21f", "#5ba965", "#ca5e4a", "#c55a9f"],
+    paletteDefault = "#919191",
+  }: { palette?: string[]; paletteDefault?: string } = {},
+): DocumentsGraph {
+  const graph = new Graph<DocumentNode>();
+  if (!projectedDocuments?.length) return graph;
+
+  const categoriesIndex: Record<string, number> = {};
+  projectedDocuments.forEach(({ document: { category } }) => {
+    categoriesIndex[category] = (categoriesIndex[category] || 0) + 1;
+  });
+  const categories = keyBy(
+    sortBy(Object.entries(categoriesIndex), [(d) => d[1]]).map(([id, count], i) => ({
+      id,
+      count,
+      color: palette[i] || paletteDefault,
+    })),
+    "id",
+  );
+
+  projectedDocuments.forEach(({ document, coordinates: { x, y } }, i) => {
+    const { category, title } = document;
+    const color = categories[category].color;
+    graph.addNode(i, { id: i + "", label: title, x, y, size: 30, color: color, document });
+  });
+
+  return graph;
+}
